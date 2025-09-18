d05a82113e04115a72db86c48a8fe0a09f89f36a
jannelledingal <j.dingal.560136@umindanao.edu.ph>
2025-09-18 13:19:59 +0800

    problem4Exam.py

--- problem4Exam.py
+++ problem4Exam.py
@@ -1,11 +1,25 @@
+def isPalindrome(valStr):
+    
+    return valStr == valStr[::-1]
 
-def isPalindrome(valueStrl):
-    if len(valueStrl) <=1:
-        return True
-    else:
-        if valueStrl[0] != valueStrl[-1]:
-            return False
+def toBinaryIfNumber(value):
+    
+    if value.isdigit():
+        return bin(int(value))[2:]  
+    return None
 
 
+user_input = input("Enter a value: ")
 
-        
+
+input_palindrome = isPalindrome(user_input)
+print("Input is a palindrome:", input_palindrome)
+
+
+binary_value = toBinaryIfNumber(user_input)
+if binary_value:
+    print("Binary equivalent:", binary_value)
+    binary_palindrome = isPalindrome(binary_value)
+    print("Binary is a palindrome:", binary_palindrome)
+else:
+    print("(No binary conversion since input is text)")
